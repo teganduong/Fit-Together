8211ce3344eb229e86387554d7568d39472906de
lcbits <lynnc24@gmail.com>
2016-06-16 22:18:08 -0700

    implement redux-react

--- src/client/app.js
+++ src/client/app.js
@@ -1,23 +1,2 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// class App extends React.Component {
-const App = () => (
-  <div>
-    React Init
-  </div>
-);
-
-ReactDOM.render(<App />, document.getElementById('app'));
-
-
-// To create a react component that has STATES, use this:
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         React Init
-//       </div>
-//     );
-//   }
-// }
