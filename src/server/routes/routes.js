590e3ca20130247601b5ee9862282e415fa0d684
Tegan Duong <teganduong@gmail.com>
2016-06-16 22:00:45 -0700

    Create routes for patients and doctors

---
+++ src/server/routes/routes.js
@@ -0,0 +1,11 @@
+const patients = require('../controllers/patientsCtrl.js');
+const doctors = require('../controllers/doctorsCtrl.js');
+
+module.exports = (app) => {
+  app.get('/api/patients', patients.getPatients);
+  app.post('/api/patients', patients.addNewPatient);
+  app.post('/api/patients/:patientId', patients.updatePatientInfo);
+
+  app.get('/api/doctors', doctors.getDoctors);
+  app.post('/api/doctors', doctors.addDoctor);
+};
