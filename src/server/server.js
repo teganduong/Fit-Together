5a7b0245c46a786685cbadac32cba8c01bd2fd11
Tegan Duong <teganduong@gmail.com>
2016-06-17 23:39:51 -0700

    Import routes into server file

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const routes = require('./routes/routes');
 const app = express();
-
 const db = require('./db/connection.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client')));
-require('./routes/routes.js')(app);
+app.use('/', routes);
 
 const port = 3000;
 app.listen(port, () => console.log('Server is listening on port ', port, '\nRefresh the browser '));
-
