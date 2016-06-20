54200d4f3461d54b7ca4dbc2639f3d298283aaca
AJ Grande <ajgrande924@gmail.com>
2016-06-20 14:56:26 -0700

    Add copy dev assets to src

--- src/client/components/Main/Main.js
+++ src/client/components/Main/Main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
 import SideNavBar from './SideNavBar';
+import PatientProfile from './PatientProfile/PatientProfile';
 
 const Main = () => (
   <div>
     <NavBar />
     <SideNavBar />
+    <PatientProfile />
   </div>
 );
 
---
+++ src/client/components/Main/PatientProfile/PatientProfile.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const PatientProfile = () => (
+	<div>
+
+	</div>
+);
+
+export default PatientProfile;
