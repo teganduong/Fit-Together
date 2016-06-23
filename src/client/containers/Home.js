9d96d70bec87ff0b38c5038701bba873057e7901
lcbits <lynnc24@gmail.com>
2016-06-23 13:34:22 -0700

    set up signup

--- src/client/containers/Home.js
+++ src/client/containers/Home.js
@@ -1,4 +1,12 @@
 import { connect } from 'react-redux';
+import { postUser } from '../actions';
 import Home from '../components/Home/Home';
 
-export default connect(null, null)(Home);
+const mapStateToProps = ({ user }) => ({ user });
+
+const mapDispatchToProps = (dispatch) => ({
+  postUser: (name, username, password, email, weight, bmi, goal, points) => dispatch(postUser(name, username, password, email, weight, bmi, goal, points)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
+
