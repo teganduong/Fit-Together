ee3d369381b3439b34c1ba970ba54b2a076052b9
AJ Grande <ajgrande924@gmail.com>
2016-06-23 11:17:56 -0700

    Save before rebase

--- src/client/components/Home/Home.js
+++ src/client/components/Home/Home.js
@@ -6,31 +6,10 @@ import LandingImage from './LandingImage';
 
 const Home = () => (
   <div>
-    <div className="site-wrapper">
-      <div className="site-wrapper-inner">
-        <div className="cover-container">
-          <div className="masthead clearfix">
-            <div className="inner">
-              <h3 className="masthead-brand">Vital Health</h3>
-              <nav>
-                <ul className="nav masthead-nav">
-                  <li><a data-toggle="modal" data-target="#loginModal">Login</a></li>
-                  <li><a data-toggle="modal" data-target="#signupModal">Signup</a></li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-          <div className="inner cover">
-            <h1 className="cover-heading">HEALTH</h1>
-            <p className="lead">
-              <a href="#" className="btn btn-lg btn-default">Learn more</a>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <HomeNavBar />
     <LoginModal />
     <SignupModal />
+    <LandingImage />
   </div>
 );
 
--- src/server/testData/seedActivities.js
+++ src/server/testData/seedActivities.js
@@ -3,49 +3,49 @@ const activitiesCtrl = require('../controllers/activitiesCtrl.js');
 const exampleActivitiesData = [
   {
     type: 'exercise',
-    category:'walking',
+    category: 'walking',
     description: 'at the park',
-    date_performed: 2016-05-24,
+    date_performed: '2016-05-24',
     count: 1200,
     units: 'steps'
   },
   {
     type: 'exercise',
-    category:'lifting',
+    category: 'lifting',
     description: 'bench press, 3 sets of 10 reps',
-    date_performed: 2016-05-24,
+    date_performed: '2016-05-24',
     count: 30,
     units: 'reps'
   },
   {
     type: 'exercise',
-    category:'running',
+    category: 'running',
     description: '3 mile run',
-    date_performed: 2016-05-26,
+    date_performed: '2016-05-26',
     count: 20,
     units: 'minutes'
   },
   {
     type: 'nutrition',
-    category:'breakfast',
+    category: 'breakfast',
     description: 'frosted flakes w/ protein shake',
-    date_performed: 2016-05-26,
+    date_performed: '2016-05-26',
     count: 650,
     units: 'calories'
   },
   {
     type: 'nutrition',
-    category:'lunch',
+    category: 'lunch',
     description: 'burger, fries, and soda',
-    date_performed: 2016-05-26,
+    date_performed: '2016-05-26',
     count: 1100,
     units: 'calories'
   },
-    {
+  {
     type: 'nutrition',
-    category:'dinner',
+    category: 'dinner',
     description: 'steak and eggs',
-    date_performed: 2016-05-26,
+    date_performed: '2016-05-26',
     count: 950,
     units: 'calories'
   },
