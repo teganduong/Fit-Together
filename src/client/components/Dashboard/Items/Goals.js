861daea0c015e508ad896041f673abacb2ea9275
Tegan Duong <teganduong@gmail.com>
2016-06-23 15:07:31 -0700

    Create Goals modal

--- src/client/components/Dashboard/Items/Goals.js
+++ src/client/components/Dashboard/Items/Goals.js
@@ -1,7 +1,44 @@
-import PlaceHolder from '../../PlaceHolder.js';
+import React, { Component } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
-const Goals = () => (
-  <PlaceHolder />
-);
+export default class Goals extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false
+    };
+  }
+
+  close() {
+    this.setState({ showModal: false });
+  }
+
+  open() {
+    this.setState({ showModal: true });
+  }
+
+  render() {
+    return (
+      <div className="static-modal">
+        <Modal.Dialog>
+          <Modal.Header>
+            <Modal.Title>Modal title</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            One fine body...
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button>Close</Button>
+            <Button bsStyle="primary">Save changes</Button>
+          </Modal.Footer>
+
+        </Modal.Dialog>
+      </div>
+    );
+  }
+}
 
 export default Goals;
--- src/client/components/Dashboard/SideNavBar.js
+++ src/client/components/Dashboard/SideNavBar.js
@@ -8,7 +8,6 @@ const SideNavBar = () => (
       <li><Link to="/dashboard">My Dashboard</Link></li>
       <li><Link to="/dashboard/teams">Teams</Link></li>
       <li><Link to="/dashboard/challenges">Challenges</Link></li>
-      <li><Link to="/dashboard/goals">Goals</Link></li>
       <li><Link to="/dashboard/stats">Stats</Link></li>
       <li><Link to="/dashboard/log">Log</Link></li>
       <li><Link to="/dashboard/tips">Tips</Link></li>
