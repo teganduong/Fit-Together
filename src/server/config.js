18d4cab72b0407975ee234f64ee8ee4927a6b706
lcbits <lynnc24@gmail.com>
2016-06-24 12:20:09 -0700

    rebase changes

---
+++ src/server/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  Fitbit: {
+    clientID: '227TQG',
+    clientSecret: '37946eb0dbfd2f22aba32f5238142068',
+    callbackURL: 'http://localhost:3000/auth/fitbit/callback'
+  },
+  Moves: {
+    clientID: 'nFod8IWw0YVBaa43incO69zA3c4aUSRy',
+    clientSecret: 'shA584APICIHSzDWcknWbake2if94aKy4ailIZ2WjY9n693bQ7p8xPjFU9tTztXu',
+    callbackURL: 'http://localhost:3000/auth/moves/callback'
+  }
+};
+
--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,121 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
-const app = express();
 const db = require('./db/connection.js');
 const passport = require('passport');
-const expressSession = require('express-session');
+const FitbitStrategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy;
+const session = require('express-session');
+const app = express();
+const config = require('./config.js');
+const MovesStrategy = require('passport-moves').Strategy;
+const usersCtrl = require('./controllers/usersCtrl.js');
 
+app.use(express.static(path.join(__dirname, '../client')));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client')));
-app.use('/', routes);
-app.use(expressSession({secret: 'FidgetyWidgets'}));
+app.use(session({ secret: 'FidgetyWidgets' }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use('/', routes);
+
+const fitbitStrategy = new FitbitStrategy({
+  clientID: config.Fitbit.clientID,
+  clientSecret: config.Fitbit.clientSecret,
+  scope: ['activity', 'heartrate', 'location', 'profile'],
+  callbackURL: config.Fitbit.callbackURL
+}, (accessToken, refreshToken, activity, done) => {
+  process.nextTick(() => {
+    const userData = {
+      name: activity._json.user.displayName,
+      username: activity._json.user.displayName,
+      password: activity._json.user.encodedId,
+      email: activity._json.user.encodedId,
+      weight: activity._json.user.weight,
+      bmi: 21.3,
+      goal: 'Live a healthy lifestyle',
+      points: 0
+    };
+  console.log('INSIDE FITBIT', userData);
+    // usersCtrl.findUser({ body: userData.name });
+
+    usersCtrl.findUser({ body: userData.name }, function(err, user) {
+      if(err) {
+        return done(err);
+      } 
+      if(user) {
+        return done(null, user);
+      } else {
+        
+        usersCtrl.addUser({ body: userData.name }, function(err, user) {
+          if(err) {
+            return done(err, null);
+          } else {
+            return done(null, user);
+          }
+        });
+    }
+  });
+
+  // done(null, {
+  //   accessToken: accessToken,
+  //   refreshToken: refreshToken,
+  //   profile: profile
+  // });
+  }
+);
+
+passport.use(fitbitStrategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+const fitbitAuthenticate = passport.authenticate('fitbit', {
+  successRedirect: '/',
+  failureRedirect: '/auth/fitbit/failure'
+});
+
+app.get('/auth/fitbit', fitbitAuthenticate);
+app.get('/auth/fitbit/callback', fitbitAuthenticate);
+app.get('/auth/fitbit/success', (req, res, next) => {
+  res.send(req.user);
+});
+
+passport.use(new MovesStrategy({
+  clientID: config.Moves.clientID,
+  clientSecret: config.Moves.clientSecret,
+  scope: ['activity', 'location'],
+  callbackURL: config.Moves.callbackURL
+},
+  (accessToken, refreshToken, profile, done) => {
+    process.nextTick(() => {
+      
+      // To keep the example simple, the user's Foursquare profile is returned
+      // to represent the logged-in user.  In a typical application, you would
+      // want to associate the Foursquare account with a user record in your
+      // database, and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+
+app.get('/auth/moves',
+  passport.authenticate('moves'));
+
+app.get('/auth/moves/callback', 
+  passport.authenticate('moves', { failureRedirect: '/login' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
