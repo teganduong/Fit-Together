16fcc76b8a5d70937ba00bef2a0218299d186bef
Tegan Duong <teganduong@gmail.com>
2016-06-26 12:06:44 -0700

    Add back signout and my dashboard links

--- src/client/components/Dashboard/SideNavBar.js
+++ src/client/components/Dashboard/SideNavBar.js
@@ -5,12 +5,14 @@ const SideNavBar = ({ username }) => (
   <div id="sidebar-wrapper">
     <p id="sidebar-title">Fit Together</p>
     <ul className="sidebar-nav">
+      <li><Link to={`/dashboard/${username}`}>My Dashboard</Link></li>
       <li><Link to={`/dashboard/${username}/settings`}>Settings</Link></li>
       <li><Link to={`/dashboard/${username}/log`}>Log</Link></li>
       <li><Link to={`/dashboard/${username}/stats`}>Stats</Link></li>
       <li><Link to={`/dashboard/${username}/challenges`}>Challenges</Link></li>
       <li><Link to={`/dashboard/${username}/teams`}>Teams</Link></li>
       <li><Link to={`/dashboard/${username}/tips`}>Tips</Link></li>
+      <li><Link to="/">Signout</Link></li>
     </ul>
   </div>
 );
