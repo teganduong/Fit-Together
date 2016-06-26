b04394f813ce3bf4fe7725b4fdd0da4e87f60383
Tegan Duong <teganduong@gmail.com>
2016-06-25 20:28:54 -0700

    Update sideBarNav formatting

--- src/client/components/Dashboard/SideNavBar.js
+++ src/client/components/Dashboard/SideNavBar.js
@@ -5,20 +5,12 @@ const SideNavBar = ({ username }) => (
   <div id="sidebar-wrapper">
     <p id="sidebar-title">Fit Together</p>
     <ul className="sidebar-nav">
-      <p className="sidebar-brand">ACCOUNT</p>
-      <ul className="sidebar-part">
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to={`/dashboard/${username}`}>My Dashboard</Link></li>
-      </ul>
-      <p className="sidebar-brand">ACTIONS</p>
-      <ul className="sidebar-part">
-        <li><Link to={`/dashboard/${username}/settings`}>Settings</Link></li>
-        <li><Link to={`/dashboard/${username}/log`}>Log</Link></li>
-        <li><Link to={`/dashboard/${username}/stats`}>Stats</Link></li>
-        <li><Link to={`/dashboard/${username}/challenges`}>Challenges</Link></li>
-        <li><Link to={`/dashboard/${username}/teams`}>Teams</Link></li>
-        <li><Link to={`/dashboard/${username}/tips`}>Tips</Link></li>
-      </ul>
+      <li><Link to={`/dashboard/${username}/settings`}>Settings</Link></li>
+      <li><Link to={`/dashboard/${username}/log`}>Log</Link></li>
+      <li><Link to={`/dashboard/${username}/stats`}>Stats</Link></li>
+      <li><Link to={`/dashboard/${username}/challenges`}>Challenges</Link></li>
+      <li><Link to={`/dashboard/${username}/teams`}>Teams</Link></li>
+      <li><Link to={`/dashboard/${username}/tips`}>Tips</Link></li>
     </ul>
   </div>
 );
