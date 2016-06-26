b3e2fc5f6edfe08c07b8d24eeccda659bb6ee4eb
Tegan Duong <teganduong@gmail.com>
2016-06-25 23:14:18 -0700

    Add error constant to redux store

--- src/client/constants/constants.js
+++ src/client/constants/constants.js
@@ -1,3 +1,2 @@
-export const UPDATE_USER = 'UPDATE_USER';
 export const RECEIVE_USER = 'RECEIVE_USER';
 export const REQUEST_ERROR = 'REQUEST_ERROR';
--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,10 +1,21 @@
 import {
-  RECEIVE_USER
+  RECEIVE_USER,
+  REQUEST_ERROR
 } from '../constants/constants.js';
 
 export const user = (state, action) => {
   switch (action.type) {
     case RECEIVE_USER: {
+      return action.data || state;
+    }
+    default:
+      return state || {};
+  }
+};
+
+export const error = (state, action) => {
+  switch (action.type) {
+    case REQUEST_ERROR: {
       return action.data || state;
     }
     default:
