23d3e3e96f8708823961d9c63cdc6d81cb501011
Tegan Duong <teganduong@gmail.com>
2016-07-01 18:29:03 -0700

    Create /entries route and methods to add and get entries from db

---
+++ src/client/api/api-keys.js
@@ -0,0 +1,1 @@
+export const GOOGLEAPI = 'AIzaSyC0G8AiJN_m4t8VWAXRXIpjF8e35iFm2-o';
---
+++ src/server/controllers/entriesCtrl.js
@@ -0,0 +1,35 @@
+const db = require('../db/connection.js');
+
+exports.addEntry = (req, res) => {
+  db.none('insert into entries(question, option1, option2)' +
+  'values(${question}, ${option1}, ${option2})',
+    req.body)
+    .then(() => {
+      res.status(201)
+        .json({
+          status: 'success',
+          message: 'Inserted entry into db',
+          data: req.body
+        });
+    })
+    .catch((err) => {
+      console.error('error in adding user: ', err);
+    });
+};
+
+exports.getEntries = (req, res) => {
+  console.log('inside getEntries in server ####');
+  db.many('select * from entries')
+    .then(entries => {
+      console.log('entries from getEntries: ', entries);
+      res.status(200)
+        .json({
+          status: 'success',
+          data: entries,
+          message: 'Retrieved all entries!'
+        });
+    })
+    .catch((err) => {
+      console.error('error in retrieving entries: ', err);
+    });
+};
