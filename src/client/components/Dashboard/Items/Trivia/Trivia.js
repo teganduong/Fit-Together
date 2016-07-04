be7b71e16c7ffcd0c50f54476cf88ab9b3f9cc58
Tegan Duong <teganduong@gmail.com>
2016-07-04 14:59:40 -0700

    Display next question when next button is clicked in Entry component

--- src/client/components/Dashboard/Items/Trivia/Trivia.js
+++ src/client/components/Dashboard/Items/Trivia/Trivia.js
@@ -2,31 +2,88 @@ import React, { Component, PropTypes } from 'react';
 import Categories from './Categories';
 import Entry from './Entry';
 
-const entry = {
-  category: 'Nutrition',
-  question: 'Carrots contain a rich source of what vitamin?',
-  options: ['C', 'D', 'A', 'B'],
-  answer: 'A'
-};
+// const entry = {
+//   category: 'Nutrition',
+//   question: 'Carrots contain a rich source of what vitamin?',
+//   options: ['C', 'D', 'A', 'B'],
+//   answer: 'A'
+// };
 
 class Trivia extends Component {
   constructor(props) {
     super(props);
+
+    this.props.fetchQuizQuestions();
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSelection = this.handleSelection.bind(this);
+    this.next = this.next.bind(this);
+    this.questions = [{ options: [] }];
+    this.entry = { options: [] };
+    this.index = 0;
+  }
+
+  componentWillMount() {
+    this.props.fetchQuizQuestions();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.questions = nextProps.questions;
+    this.entry = nextProps.question;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('submit button clicked!');
+    // console.log('this.state.selectedOption: ', this.state.selectedOption);
+    // const { selectedOption, entry } = this.state;
+    // if (selectedOption === entry.answer) {
+    //   console.log('answer correct!');
+    // } else {
+    //   console.log('answer wrong!');
+    // }
+  }
+
+  handleSelection(event) {
+    console.log('event.target.value for handleSelection: ', event.target.value);
+  }
+
+  // next question
+  next() {
+    const { receiveCurrentQuestion } = this.props;
+    this.index++;
+    console.log('next index: ', this.index);
+    console.log('next question: ', this.questions[this.index]);
+    const nextQuestion = this.questions[this.index];
+    if (nextQuestion) {
+      receiveCurrentQuestion(nextQuestion);
+    }
   }
 
   render() {
+    console.log('this.entry in Trivia: ', this.entry);
+
     return (
       <div className="main-container">
         <Categories />
-        <Entry entry={entry} />
+        <Entry 
+          entry={this.questions[this.index]}
+          handleSelection={this.handleSelection}
+          handleSubmit={this.handleSubmit} 
+          next={this.next}
+        />
       </div>
     );
   }
 }
 
-// Trivia.propTypes = {
-//   entries: PropTypes.array,
-//   getEntries: PropTypes.func
-// };
+Trivia.propTypes = {
+  entries: PropTypes.array,
+  getEntries: PropTypes.func,
+  fetchQuizQuestions: PropTypes.func,
+  questions: PropTypes.array,
+  question: PropTypes.object,
+  receiveCurrentQuestion: PropTypes.func
+};
 
 export default Trivia;
--- src/client/containers/Trivia.js
+++ src/client/containers/Trivia.js
@@ -1,10 +1,15 @@
 import { connect } from 'react-redux';
 import Trivia from '../components/Dashboard/Items/Trivia/Trivia';
-import { getEntries } from '../actions';
+import { getEntries, selectOption, fetchQuizQuestions, receiveCurrentQuestion } from '../actions';
 
-const mapStateToProps = ({ entries }) => ({ entries });
+const mapStateToProps = ({ questions, question, selectedOption }) => 
+  ({ questions, question, selectedOption });
+
 const mapDispatchToProps = (dispatch) => ({
-  getEntries: () => dispatch(getEntries())
+  getEntries: () => dispatch(getEntries()),
+  selectOption: (option) => dispatch(selectOption(option)),
+  fetchQuizQuestions: () => dispatch(fetchQuizQuestions()),
+  receiveCurrentQuestion: (question) => dispatch(receiveCurrentQuestion(question))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Trivia);
