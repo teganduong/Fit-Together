8a48fdc4fb020680d40d8f93d161cad9ce572069
Tegan Duong <teganduong@gmail.com>
2016-07-04 19:55:35 -0700

    Seed mongodb with fitness category questions

--- src/server/testData/seedQuestions.js
+++ src/server/testData/seedQuestions.js
@@ -74,6 +74,113 @@ const entriesData = [
     question: 'How many calories are in 1 gram of sugar?',
     options: [5, 8, 4, 3],
     answer: 4
+  },
+  {
+    category: 'Fitness',
+    question: 'The expression of power can best be described as:',
+    options: [
+      'Force x Speed x Time x Effort',
+      'Speed x Force divided by Momentum',
+      'Distance moved divided by Speed x Volume',
+      'Force x Distance divided by Time'
+    ],
+    answer: 'Force x Distance divided by Time'
+  },
+  {
+    category: 'Fitness',
+    question: 'If you do both weight training and cardio in the same session, which should come first?',
+    options: [
+      'weight training',
+      'cardio'
+    ],
+    answer: 'weight training'
+  },
+  {
+    category: 'Fitness',
+    question: 'If you just train the quads while ignoring the opposing muscle, you might:',
+    options: [
+      'tear your achilles',
+      'get a stinger',
+      'pull your hamstring and end up with severe muscle imbalance',
+      'end up with a hip pointer'
+    ],
+    answer: 'pull your hamstring and end up with severe muscle imbalance'
+  },
+  {
+    category: 'Fitness',
+    question: 'Which of the following situations should be avoided when programming resistance training for children?',
+    options: [
+      'performing overhead lifting activities',
+      'utilizing free weights for resistance',
+      'lifting resistance loads greater than 10RM'
+    ],
+    answer: 'lifting resistance loads greater than 10RM'
+  },
+  {
+    category: 'Fitness',
+    question: 'If you are performing plyometric exercises while training, you are performing:',
+    options: [
+      'explosive movements',
+      'slow deliberate movements',
+      'twisting movements',
+      'slow breathing techniques'
+    ],
+    answer: 'explosive movements'
+  },
+  {
+    category: 'Fitness',
+    question: 'Initial strength gains seen during the first few weeks of a resistance-training program are primarily attributed to:',
+    options: [
+      'changes in Type I muscle fiber to Type II muscle fibers',
+      'increases in the cross sectional size of the muscle',
+      'neural adaptations',
+      'weight gain'
+    ],
+    answer: 'neural adaptations'
+  },
+  {
+    category: 'Fitness',
+    question: 'What is the name of the diamond shaped muscle in the back that is worked when rowing?',
+    options: [
+      'Biceps',
+      'Trapezius',
+      'Abdominals',
+      'Hamstrings'
+    ],
+    answer: 'Trapezius'
+  },
+  {
+    category: 'Fitness',
+    question: 'What type of exercise uses body weight for resistance in a rhythmic manner?',
+    options: [
+      'Calisthenics',
+      'Yoga',
+      'Pilates',
+      'Weight Lifting'
+    ],
+    answer: 'Calisthenics'
+  },
+  {
+    category: 'Fitness',
+    question: 'What is hypertrophy?',
+    options: [
+      'Increasing Calorie Intake',
+      'Increasing Muscle Fiber',
+      'Increasing Fat',
+      'Increasing Your Workout'
+    ],
+    answer: 'Increasing Muscle Fiber'
+  },
+  {
+    category: 'Fitness',
+    question: 'What is it called when your body converts fat into energy?',
+    options: [
+      'Overtraining',
+      'Hypertrophy',
+      'Flexibility',
+      'Ketosis'
+    ],
+    answer: 'Ketosis'
   }
 ];
 
