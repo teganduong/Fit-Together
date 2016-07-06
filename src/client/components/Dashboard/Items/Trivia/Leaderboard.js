4b6555c2f6e13323e8df4c70d052c65ec64be8d1
Tegan Duong <teganduong@gmail.com>
2016-07-05 19:48:25 -0700

    Format and style leaderboard with dummy data

--- src/client/components/Dashboard/Items/Trivia/Leaderboard.js
+++ src/client/components/Dashboard/Items/Trivia/Leaderboard.js
@@ -1,9 +1,60 @@
 import React, { PropTypes } from 'react';
 
-const Leaderboard = () => (
-  <div className="leaderboard col-sm-3">
-    <h3>Leaderboard</h3>
-  </div>
-);
+const users = [
+  {
+    name: 'Jessica Jones',
+    username: 'jjones',
+    points: 160,
+    user_icon: 'https://assets-cdn.github.com/images/modules/styleguide/linktocat.png',
+    rank: 1
+  },
+  {
+    name: 'John Jacobs',
+    username: 'johnyj',
+    points: 156,
+    user_icon: 'https://octodex.github.com/images/gangnamtocat.png',
+    rank: 2
+  },
+  {
+    name: 'Jeffrey Jordan',
+    username: 'jeffj',
+    points: 142,
+    user_icon: 'https://stevegrunwell.github.io/wordpress-git/assets/heisencat.png',
+    rank: 3
+  },
+  {
+    name: 'Alex Adams',
+    username: 'alexa',
+    points: 133,
+    user_icon: 'https://assets-cdn.github.com/images/modules/styleguide/linktocat.png',
+    rank: 4
+  }
+];
+
+const Leaderboard = () => {
+  const leaderboard = users.map(user => (
+    <tr>
+      <td className="user-rank">{user.rank}</td>
+      <td><img className="user-img" src={user.user_icon} alt="" /></td>
+      <td className="user-name">{user.name}</td>
+      <td>{user.points}</td>
+    </tr>
+  ));
+
+  return (
+    <div className="leaderboard col-sm-4">
+      <h3 className="leaderboard-title">Leaderboard</h3>
+      <table>
+        <tr className="leaderboard-head">
+          <th>Rank</th>
+          <th></th>
+          <th>Name</th>
+          <th>Points</th>
+        </tr>
+        {leaderboard}
+      </table>
+    </div>
+  );
+};
 
 export default Leaderboard;
